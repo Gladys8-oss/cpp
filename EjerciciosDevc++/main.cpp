3ddeb0a876f848313001f83c2eda16e24c1f4b3c
Gladys Sosa <gladyssosa222@gmail.com>
2021-02-02 22:40:46 -0600

    agregando ejrecicio 4 y 5

---
+++ EjerciciosDevc++/main.cpp
@@ -0,0 +1,18 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	string Nombre;
+	
+	cout << "Ingrese el nombre: ";
+	cin>> Nombre;
+	
+	cout<<endl;
+	cout<< "Su nombre es: "<< Nombre;
+	cout<<endl;
+	system("pause");
+	return 0;
+}
