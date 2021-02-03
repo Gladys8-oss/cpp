bc0c7c731111dc15a2cf82aea521eff9b52a4df1
Gladys Sosa <gladyssosa222@gmail.com>
2021-02-02 21:26:04 -0600

    agregando ejercicio 2

---
+++ Ejercicios/Ejercicio 2/mensajes.cpp
@@ -0,0 +1,18 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    string Nombre;
+
+    cout << "Ingrese su nombre: ";
+    cin >> Nombre;
+
+    cout<<endl;
+	cout << "Hola mi nombre es: "<<Nombre;
+    cout<<endl;
+	system("pause");
+	return 0;
+}
