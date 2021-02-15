18c07e1cd1ad01c336aaa2b6886d7499d9a4df40
Gladys Sosa <gladyssosa222@gmail.com>
2021-02-15 13:50:17 -0600

    subiendo Biblioteca modificacion

--- Ejercicios/Ejercicio02 - BIBLIOTECA/main.cpp
+++ Ejercicios/Ejercicio02 - BIBLIOTECA/main.cpp
@@ -9,14 +9,14 @@ void cargarLibros() {
 
 	srand (time(NULL));
 
-    libros [ 0 ] [ 0 ] = " Algoritmos " ; libros [ 0 ] [ 1 ] = " Algoritmos y Programacion (Guia para docentes) ", libros [ 0 ] [ 2] = "Autor" ;
-	libros [ 1 ] [ 0 ] = " Algoritmos " ; libros [ 1 ] [ 1 ] = " Apuntes de Algoritmos y Estructuras de Datos " , libros [ 1 ] [ 2] = " James ";
+    libros [ 0 ] [ 0 ] = " Algoritmos " ; libros [ 0 ] [ 1 ] = " Algoritmos y Programacion (Guia para docentes) "; libros [ 0 ] [ 2] = "Yari" ;
+	libros [ 1 ] [ 0 ] = " Algoritmos " ; libros [ 1 ] [ 1 ] = " Apuntes de Algoritmos y Estructuras de Datos " ; libros [ 1 ] [ 2] = " James ";
 	libros [ 2 ] [ 0 ] = " Algoritmos " ; libros [ 2 ] [ 1 ] = " Breves Notas sobre Análisis de Algoritmos " ; libros [ 2 ] [ 2 ] = " Juan ";
 	libros [ 3 ] [ 0 ] = " Algoritmos " ; libros [ 3 ] [ 1 ] = " Fundamentos de Informatica y Programacion " ; libros [ 3 ] [ 2 ] = " Carlos ";
 	libros [ 4 ] [ 0 ] = " Algoritmos " ; libros [ 4 ] [ 1 ] = " Temas selectos de estructuras de datos " ; libros [ 4 ] [ 2 ] = " Javier ";
 	libros [ 5 ] [ 0 ] = " Algoritmos " ; libros [ 5 ] [ 1 ] = " Teoria sintactico-gramatical de objetos " ; libros [ 5 ] [ 2 ] = " Gladys ";
 	libros [ 6 ] [ 0 ] = " Base de Datos " ; libros [ 6 ] [ 1 ] = " Apuntes de Base de Datos 1 " ; libros [ 6 ] [ 2] = " Cecilia ";
-	libros [ 7 ] [ 0 ] = " Base de Datos " ; libros [ 7 ] [ 1 ] = " Base de Datos (2005) " ; libros [ 7 ] [ 2 ] = " Autor ";
+	libros [ 7 ] [ 0 ] = " Base de Datos " ; libros [ 7 ] [ 1 ] = " Base de Datos (2005) " ; libros [ 7 ] [ 2 ] = " Santos";
 	libros [ 8 ] [ 0 ] = " Base de Datos " ; libros [ 8 ] [ 1 ] = " Base de Datos (2011) " ; libros [ 8 ] [ 2 ] = " Maribel ";
 	libros [ 9 ] [ 0 ] = " Base de Datos " ; libros [ 9 ] [ 1 ] = " Base de Datos Avanzadas (2013) " ; libros [ 9 ] [ 2 ] = " Kenneth ";
 	libros [ 10 ] [ 0 ] = " Base de Datos " ; libros [ 10 ] [ 1 ] = " Diseño Conceptual de Bases de Datos " ; libros [ 10 ] [ 2 ] = " Mirna ";
@@ -24,30 +24,30 @@ void cargarLibros() {
 	libros [ 12 ] [ 0 ] = " Ciencia Computacional " ; libros [ 12 ] [ 1 ] = " Breves Notas sobre Teoria de la Computación " ; libros [ 12 ] [ 2 ] = " Cindy ";
 	libros [ 13 ] [ 0 ] = " Metodologias de desarrollo de software " ; libros [ 13 ] [ 1 ] = " Compendio de Ingenieria del Software " ; libros [ 13 ] [ 2 ] = " Maria ";
 	libros [ 14 ] [ 0 ] = " Metodologias de desarrollo de software " ; libros [ 14 ] [ 1 ] = " Diseno agil con TDD " ; libros [ 14] [ 2 ] = " Mabel ";
-	libros [ 15 ] [ 0 ] = " Metodologias de desarrollo de software " ; libros [ 15 ] [ 1 ] = " Ingeniería de Software: Una Guía para Crear Sistemas de Información " ; libros [ 15 ] [ 2 ] = " Autor";
-	libros [ 16 ] [ 0 ] = " Metodologias de desarrollo de software " ; libros [ 16 ] [ 1 ] = " Scrum & Extreme Programming (para programadores) " ; libros [ 16 ] [ 2 ] = " Autor ";
-	libros [ 17 ] [ 0 ] = " Metodologias de desarrollo de software " ; libros [ 17 ] [ 1 ] = " Scrum y XP desde las trincheras " ; libros [ 17 ] [ 2 ] = " Autor ";
-	libros [ 18 ] [ 0 ] = " Miscelaneos " ; libros [ 18 ] [ 1 ] = " 97 cosas que todo programador deberia saber " ; libros [ 18 ] [ 2 ] = " Autor ";
-	libros [ 19 ] [ 0 ] = " Miscelaneos " ; libros [ 19 ] [ 1 ] = " Docker " ; libros [ 19 ] [ 2 ] = " Autor ";
-	libros [ 20 ] [ 0 ] = " Miscelaneos " ; libros [ 20 ] [ 1 ] = " El camino a un mejor programador " ; libros [ 20 ] [ 2 ] = " Autor ";
-	libros [ 21 ] [ 0 ] = " Miscelaneos " ; libros [ 21 ] [ 1 ] = " Introduccion a Docker " ; libros [ 21 ] [ 2 ] = " Autor ";
-	libros [ 22 ] [ 0 ] = " Miscelaneos " ; libros [ 22 ] [ 1 ] = " Programación de videojuegos SDL " ; libros [ 22 ] [ 2 ] = " Autor ";
-	libros [ 23 ] [ 0 ] = " PHP " ; libros [ 23 ] [ 1 ] = " Manual de estudio introductorio al lenguaje PHP procedimental " ; libros [ 23 ] [ 2 ] = " Autor ";
-	libros [ 24 ] [ 0 ] = " PHP " ; libros [ 24 ] [ 1 ] = " PHP y Programación orientada a objetos " ; libros [ 24 ] [ 2 ] = " Autor ";
-	libros [ 25 ] [ 0 ] = " PHP " ; libros [ 25 ] [ 1 ] = " POO y MVC en PHP " ; libros [ 25 ] [ 2 ] = " Autor ";
-	libros [ 26 ] [ 0 ] = " PHP " ; libros [ 26 ] [ 1 ] = " Silex, el manual oficial " ; libros [ 26 ] [ 2 ] = " Autor ";
-	libros [ 27 ] [ 0 ] = " PHP " ; libros [ 27 ] [ 1 ] = " Symfony 1.4, la guia definitiva " ; libros [ 27 ] [ 2 ] = " Autor ";
-	libros [ 28 ] [ 0 ] = " PHP " ; libros [ 28 ] [ 1 ] = " Symfony 2.4, el libro oficial " ; libros [ 28 ] [ 2 ] = " Autor ";
-	libros [ 29 ] [ 0 ] = " Python " ; libros [ 29 ] [ 1 ] = " Aprenda a pensar como un programador (con Python) " ; libros [ 29 ] [ 2 ] = " Autor ";
-	libros [ 30 ] [ 0 ] = " Python " ; libros [ 30 ] [ 1 ] = " Doma de Serpientes para Ninos: Aprendiendo a Programar con Python " ; libros [ 30 ] [ 2 ] = " Autor ";
-	libros [ 31 ] [ 0 ] = " Python " ; libros [ 31 ] [ 1 ] = " Inmersión en Python " ; libros [ 31 ] [ 2 ] = " Autor ";
-	libros [ 32 ] [ 0 ] = " Python " ; libros [ 32 ] [ 1 ] = " Inmersión en Python 3 " ; libros [ 32 ] [ 2 ] = " Autor ";
-	libros [ 33 ] [ 0 ] = " Python " ; libros [ 33 ] [ 1 ] = " Introduccion a la programacion con Python " ; libros [ 33 ] [ 2 ] = " Autor ";
-	libros [ 34 ] [ 0 ] = " Python " ; libros [ 34 ] [ 1 ] = " Introducción a Programando con Python " ; libros [ 34 ] [ 2 ] = " Autor ";
-	libros [ 35 ] [ 0 ] = " Python " ; libros [ 35 ] [ 1 ] = " Python instantaneo (1999) " ; libros [ 35 ] [ 2 ] = " Autor ";
-	libros [ 36 ] [ 0 ] = " Python " ; libros [ 36 ] [ 1 ] = " Python para ciencia e ingenieria " ; libros [ 36 ] [ 2 ] = " Autor ";
-	libros [ 37 ] [ 0 ] = " Python " ; libros [ 37 ] [ 1 ] = " Python para principiantes " ; libros [ 37 ] [ 2 ] = " Autor ";
-	libros [ 38 ] [ 0 ] = " Python " ; libros [ 38 ] [ 1 ] = " Python para todos " ; libros [ 38 ] [ 2 ] = " Autor ";
+	libros [ 15 ] [ 0 ] = " Metodologias de desarrollo de software " ; libros [ 15 ] [ 1 ] = " Ingeniería de Software: Una Guía para Crear Sistemas de Información " ; libros [ 15 ] [ 2 ] = " Jenny";
+	libros [ 16 ] [ 0 ] = " Metodologias de desarrollo de software " ; libros [ 16 ] [ 1 ] = " Scrum & Extreme Programming (para programadores) " ; libros [ 16 ] [ 2 ] = " Ender ";
+	libros [ 17 ] [ 0 ] = " Metodologias de desarrollo de software " ; libros [ 17 ] [ 1 ] = " Scrum y XP desde las trincheras " ; libros [ 17 ] [ 2 ] = " Roger ";
+	libros [ 18 ] [ 0 ] = " Miscelaneos " ; libros [ 18 ] [ 1 ] = " 97 cosas que todo programador deberia saber " ; libros [ 18 ] [ 2 ] = " Awner ";
+	libros [ 19 ] [ 0 ] = " Miscelaneos " ; libros [ 19 ] [ 1 ] = " Docker " ; libros [ 19 ] [ 2 ] = " Hermelindo ";
+	libros [ 20 ] [ 0 ] = " Miscelaneos " ; libros [ 20 ] [ 1 ] = " El camino a un mejor programador " ; libros [ 20 ] [ 2 ] = " Blanca ";
+	libros [ 21 ] [ 0 ] = " Miscelaneos " ; libros [ 21 ] [ 1 ] = " Introduccion a Docker " ; libros [ 21 ] [ 2 ] = " Zulmy ";
+	libros [ 22 ] [ 0 ] = " Miscelaneos " ; libros [ 22 ] [ 1 ] = " Programación de videojuegos SDL " ; libros [ 22 ] [ 2 ] = " Noemy ";
+	libros [ 23 ] [ 0 ] = " PHP " ; libros [ 23 ] [ 1 ] = " Manual de estudio introductorio al lenguaje PHP procedimental " ; libros [ 23 ] [ 2 ] = " Miriam ";
+	libros [ 24 ] [ 0 ] = " PHP " ; libros [ 24 ] [ 1 ] = " PHP y Programación orientada a objetos " ; libros [ 24 ] [ 2 ] = " Izel ";
+	libros [ 25 ] [ 0 ] = " PHP " ; libros [ 25 ] [ 1 ] = " POO y MVC en PHP " ; libros [ 25 ] [ 2 ] = " Elizabeth ";
+	libros [ 26 ] [ 0 ] = " PHP " ; libros [ 26 ] [ 1 ] = " Silex, el manual oficial " ; libros [ 26 ] [ 2 ] = " Anthony ";
+	libros [ 27 ] [ 0 ] = " PHP " ; libros [ 27 ] [ 1 ] = " Symfony 1.4, la guia definitiva " ; libros [ 27 ] [ 2 ] = " Suyapa ";
+	libros [ 28 ] [ 0 ] = " PHP " ; libros [ 28 ] [ 1 ] = " Symfony 2.4, el libro oficial " ; libros [ 28 ] [ 2 ] = " Pedro ";
+	libros [ 29 ] [ 0 ] = " Python " ; libros [ 29 ] [ 1 ] = " Aprenda a pensar como un programador (con Python) " ; libros [ 29 ] [ 2 ] = " Daniel ";
+	libros [ 30 ] [ 0 ] = " Python " ; libros [ 30 ] [ 1 ] = " Doma de Serpientes para Ninos: Aprendiendo a Programar con Python " ; libros [ 30 ] [ 2 ] = "Eduardo ";
+	libros [ 31 ] [ 0 ] = " Python " ; libros [ 31 ] [ 1 ] = " Inmersión en Python " ; libros [ 31 ] [ 2 ] = " Jose ";
+	libros [ 32 ] [ 0 ] = " Python " ; libros [ 32 ] [ 1 ] = " Inmersión en Python 3 " ; libros [ 32 ] [ 2 ] = " David ";
+	libros [ 33 ] [ 0 ] = " Python " ; libros [ 33 ] [ 1 ] = " Introduccion a la programacion con Python " ; libros [ 33 ] [ 2 ] = " Enrique ";
+	libros [ 34 ] [ 0 ] = " Python " ; libros [ 34 ] [ 1 ] = " Introducción a Programando con Python " ; libros [ 34 ] [ 2 ] = " Antonia ";
+	libros [ 35 ] [ 0 ] = " Python " ; libros [ 35 ] [ 1 ] = " Python instantaneo (1999) " ; libros [ 35 ] [ 2 ] = " Claudia ";
+	libros [ 36 ] [ 0 ] = " Python " ; libros [ 36 ] [ 1 ] = " Python para ciencia e ingenieria " ; libros [ 36 ] [ 2 ] = " Jeffrey ";
+	libros [ 37 ] [ 0 ] = " Python " ; libros [ 37 ] [ 1 ] = " Python para principiantes " ; libros [ 37 ] [ 2 ] = " Carlos ";
+	libros [ 38 ] [ 0 ] = " Python " ; libros [ 38 ] [ 1 ] = " Python para todos " ; libros [ 38 ] [ 2 ] = " Ramon ";
 }
 
 int main(int argc, char *argv[])
@@ -92,7 +92,7 @@ int main(int argc, char *argv[])
 		}
 		
 		if (salir == false) {
-			char continuar == 'n';
+			char continuar = 'n';
 
 			while (true)
 			{
